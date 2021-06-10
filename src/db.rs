6995f3d750a9f24ba293ee5bbfa28e149387b001
Niels Sonnich Poulsen <nielssonnich@gmail.com>
2021-06-10 22:45:43 +0200

    Working install and comment list.

--- src/db.rs
+++ src/db.rs
@@ -1,4 +1,5 @@
 use serde::{Deserialize, Serialize};
+use thiserror::Error;
 
 #[derive(Serialize)]
 pub struct Comment {
@@ -14,11 +15,52 @@ pub enum Repo {
     SqliteRepo(SqlitePool),
 }
 
+#[derive(Error, Debug)]
+pub enum RepoError {
+    #[error("r2d2 error")]
+    R2d2Error(#[from] r2d2::Error),
+    #[error("sqlite error")]
+    SqliteError(#[from] rusqlite::Error),
+}
+
 impl Repo {
-    pub fn get_comments(&self) -> actix_web::Result<Vec<Comment>> {
+    pub fn install(&self) -> Result<(), RepoError> {
         match self {
             Repo::SqliteRepo(pool) => {
-                actix_web::Result::Ok(vec![])
+                let conn = pool.get()?;
+                let mut stmt = conn.prepare("pragma table_info('comments')")?;
+                let mut rows = stmt.query([])?;
+                if rows.next()?.is_none() {
+                    println!("installing sqlite3 database...");
+                    let mut create = conn.prepare(
+                        "create table comments (
+                            id integer primary key autoincrement,
+                            name text(100) not null,
+                            content text not null
+                        )"
+                    )?;
+                    create.execute([])?;
+                }
+                Ok(())
+            },
+        }
+    }
+
+    pub fn get_comments(&self) -> Result<Vec<Comment>, RepoError> {
+        match self {
+            Repo::SqliteRepo(pool) => {
+                let conn = pool.get()?;
+                let mut stmt = conn.prepare("select id, name, content from comments")?;
+                let mut rows = stmt.query([])?;
+                let mut comments = Vec::new();
+                while let Some(row) = rows.next()? {
+                    comments.push(Comment {
+                        id: row.get(0)?,
+                        name: row.get(1)?,
+                        content: row.get(2)?,
+                    });
+                }
+                actix_web::Result::Ok(comments)
             },
         }
     }
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{App, HttpResponse, HttpServer, Responder, error::InternalError, get, http::StatusCode, post, web};
 use db::{Repo, SqlitePool};
 use r2d2_sqlite::SqliteConnectionManager;
 
 mod db;
 
 #[get("/")]
-async fn hello(repo: web::Data<Repo>) -> actix_web::Result<HttpResponse> {
-    let result = repo.get_comments();
-    result.map(|comments| {
-        HttpResponse::Ok().json(comments)
-    })
+async fn get_comments(repo: web::Data<Repo>) -> actix_web::Result<HttpResponse> {
+    let comments = repo.get_comments().map_err(|e| {
+        InternalError::new(e, StatusCode::INTERNAL_SERVER_ERROR)
+    })?;
+    Ok(HttpResponse::Ok().json(comments))
 }
 
 #[actix_web::main]
@@ -21,10 +21,12 @@ async fn main() -> std::io::Result<()> {
     let pool = SqlitePool::new(manager).unwrap();
     let repo: Repo = Repo::SqliteRepo(pool);
 
+    repo.install().unwrap();
+
     HttpServer::new(move || {
         App::new()
             .data(repo.clone())
-            .service(hello)
+            .service(get_comments)
     })
     .bind("127.0.0.1:5000")?
     .run()
